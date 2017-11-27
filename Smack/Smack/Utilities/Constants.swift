cc0fd6f6ce02b8318d87a76a82b4a447333896b3
iTheNextG <you@example.com>
2017-11-27 11:56:06 +0300

    lesson9

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+
+//URL Constant
+
+let BASE_URL = "https://chattychatchatc.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 
 let TO_LOGIN = "toLogin"
 let TO_CREATEACCOUNT = "toCreatAccount"
 let UNWIND = "unwindToChannel"
+
+
+// user defaults
+let TOKEN_KEY = "token"
+let LOGGEDIN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
