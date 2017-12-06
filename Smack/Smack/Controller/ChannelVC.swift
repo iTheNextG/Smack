5bde66aff5215a4483ac0d90810902cc4cdbd6ea
iTheNextG <you@example.com>
2017-12-06 11:24:47 +0300

    end of lesson 16

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -11,13 +11,15 @@ import UIKit
 class ChannelVC: UIViewController {
 
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60 
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTE_USER_DATA_DID_CHANGE, object: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +28,16 @@ class ChannelVC: UIViewController {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
     
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarNAme)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        }else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named : "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
 
 }
