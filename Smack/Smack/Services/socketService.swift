5442266d2b63765d2afec6302f0c7be29af39496
iTheNextG <you@example.com>
2017-12-10 13:27:17 +0300

    lesson22

---
+++ Smack/Smack/Services/socketService.swift
@@ -0,0 +1,13 @@
+//
+//  socketService.swift
+//  Smack
+//
+//  Created by TheNextG on 12/8/17.
+//  Copyright © 2017 TheNextG. All rights reserved.
+//
+
+import UIKit
+
+class socketService: NSObject {
+
+}
